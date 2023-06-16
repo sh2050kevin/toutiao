130c403cb3fcbbbabe040b290903e7a989f85b59
sh2050kevin <40140645+sh2050kevin@users.noreply.github.com>
2023-06-16 20:52:14 +0800

    新增vuex模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import catagory from './modules/catagory'
+import newlist from './modules/newlist'
 
 Vue.use(Vuex)
 
@@ -13,5 +15,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    catagory,
+    newlist
   }
 })
